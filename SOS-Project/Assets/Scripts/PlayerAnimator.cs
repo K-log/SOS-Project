ba1828885608c66c43a64d308e8b0bb38ebd2e80
K-Log <noahb343@gmail.com>
2018-01-14 01:50:51 -0800

    Debug player animations/movement
    Debug player animations setup but not working. Movement system is working.

---
+++ SOS-Project/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour {
+
+    private Animator anim;
+    
+    // Use this for initialization
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W)) // Move the player up
+        {
+            anim.SetBool("WalkUp", true);
+            anim.SetBool("WalkDown", false);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.A)) // Move the player left
+        {
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkDown", false);
+            anim.SetBool("WalkLeft", true);
+            anim.SetBool("WalkRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.S)) // Move the player down
+        {
+            anim.SetBool("WalkUp", false);
+            anim.SetBool("WalkDown", true);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", false);
+        }
+
+        if (Input.GetKey(KeyCode.D)) // Move the player right
+        {
+            anim.SetBool("WalkUp", true);
+            anim.SetBool("WalkDown", false);
+            anim.SetBool("WalkLeft", false);
+            anim.SetBool("WalkRight", true);
+        }
+
+
+
+    }
+}
